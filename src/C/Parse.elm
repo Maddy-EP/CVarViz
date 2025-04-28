389e30bf9bd88a362f5d340a9d908fbbc9182bd5
Maddy EP <maddyep@proton.me>
2025-04-27 17:55:02 -0700

    src/C/Parse: Implement parsers for C definitions.
    Very simple, for the time being: Only parse definitions of ints and
    arbitrarily-deep nesting of references / pointeres to ints.
    
    The only expression forms supported right now are address-of and dereference,
    which allow for the construction of interesting points-to graphs.

---
+++ src/C/Parse.elm
@@ -0,0 +1,68 @@
+module C.Parse exposing (parseDef)
+
+import C.Def exposing (Def)
+import C.Env exposing (Env)
+import C.Exp exposing (Exp(..))
+import C.Type exposing (Type(..))
+import Char as C
+import Parser exposing (..)
+import Set as S
+
+
+parseType : Parser Type
+parseType =
+    succeed identity
+        |. keyword "int"
+        |. spaces
+        |= loop identity parseStars
+        |= succeed IntT
+
+
+parseStars : (Type -> Type) -> Parser (Step (Type -> Type) (Type -> Type))
+parseStars appPointerT =
+    oneOf
+        [ succeed (Loop (PointerT << appPointerT)) |. symbol "*"
+        , succeed (Done appPointerT)
+        ]
+
+
+parseVarName : Parser String
+parseVarName =
+    variable
+        { start = C.isAlpha
+        , inner = \c -> C.isAlphaNum c || c == '_'
+        , reserved = S.fromList [ "int", "null" ]
+        }
+
+
+parseDef : Env -> Parser Def
+parseDef env =
+    succeed Def
+        |. spaces
+        |= parseType
+        |. spaces
+        |= parseVarName
+        |. spaces
+        |. symbol "="
+        |. spaces
+        |= parseExp env
+        |. spaces
+        |. symbol ";"
+        |. spaces
+        |. end
+
+
+parseExp : Env -> Parser Exp
+parseExp env =
+    oneOf
+        [ map Lit int
+        , map (always Null) (keyword "null")
+        , succeed Addr
+            |. symbol "&"
+            |= parseVarName
+        , succeed Deref
+            |. symbol "*"
+            |= lazy (\_ -> parseExp env)
+        , succeed Var
+            |= parseVarName
+        ]
